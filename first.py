d525b18634600f032102387a00ccc16b1c82e036
Shkkhwaja <khwajashaikh703@gmail.com>
2026-07-20 23:37:16 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,104 @@
+# num = int(input("Enter a number: "))    
+
+# sum=0
+
+# for i in range(1,num+1):
+#     sum = sum + i
+# print(sum)
+
+
+# fac = 1
+
+# for i in range(1,num +1):
+#     fac = fac*i
+# print(fac)
+
+
+# for i in range(1,num +1):
+#     if i % 2 == 0:
+#         print(i)
+
+# for i in range(1,num +1):
+#     if i % 2 == 1:
+#         print(i)    
+
+# for i in range(1,10+1):
+#     print(f"{num} x {i} = {num*i}")
+
+# for i in range(num,0,-1):
+#     print(i)
+
+
+# count = 0
+
+# while num > 0:
+#     num=num //10
+#     count = count +1
+# print(count)
+
+
+# sum =1
+
+# for i in range(1,num +1):
+#     sum = sum * i
+# print(sum)
+
+
+
+
+
+
+# st = input("Enter string : ")
+
+# for i in range(len(st) -1,-1,-1):
+#     print(st[i], end="")
+
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print("*", end="")
+#     print()
+
+
+
+num = int(input("Enter number : "))
+
+# n = 0
+
+# while n <= num:
+#     print(n)
+#     n += 1
+
+# while num >= 1:
+#     print(num)
+
+#     num -= 1
+
+# i = 1
+
+# while i <= 10:
+#     print(f"{num} x {num + i} = {num * i}")
+#     i += 1
+    
+
+count= 0
+
+for i in num:
+    count = count + 1
+
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
