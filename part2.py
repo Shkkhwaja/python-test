4600aa02bb1cacc1b98f6b33d14c0c4b7d5bec96
Shkkhwaja <khwajashaikh703@gmail.com>
2026-07-23 23:05:17 +0530

    list with functions

---
+++ part2.py
@@ -0,0 +1,123 @@
+# fruits = ["Apple","banana","mango","Orange"]
+
+# print(fruits)
+
+# n = [12,13,14,15,16]
+
+# print(n[0])
+# print(n[-1])
+# print(n[2])
+
+
+# n1=int(input("enter number"))
+# n2=int(input("enter number"))
+# n3=int(input("enter number"))
+# n4=int(input("enter number"))
+# n5=int(input("enter number"))
+
+
+# n =[n1,n2,n3,n4,n5]
+
+# max=0
+
+# for i in n:
+#     sum = sum + i
+
+
+# n=[]
+
+# for i in range(5):
+#     num = int(input(f"Enter number{i + 1} : "))
+#     n.append(num)
+
+# largest = n[0]
+
+# for num in n:
+#     if num > largest:
+#         largest = num
+
+# print("largest number",largest)
+
+
+
+# n=[1, 2, 3, 4, 6]
+
+# odd = 0
+# even=0
+
+
+# for i in range(0,len(n)):
+#     if n[i] % 2 == 0:
+#       even +=1  
+#     else:
+#        odd +=1
+
+# print("Odd number ",odd)
+# print("even number ",even)
+
+
+
+# n=[]
+
+# for i in range(5):
+#     num = int(input(f"Enter number {i +1} : "))
+#     n.append(num)
+
+# for j in range(len(n) -1,-1,-1):
+#     print(n[j],end=" ")
+
+
+
+# n=[1, 2, 3, 4, 5, 6]
+
+# odd=[]
+
+# for i in range(0,len(n)):
+#     if n[i] % 2 == 1:
+#         odd.append(n[i])
+        
+
+# print(odd)
+
+
+
+
+n=[10,20,50,40,60]
+
+n.append(70)
+n.extend([80,90])
+n.insert(0,20)
+n.remove(20)
+
+n.pop()
+n.pop(2)
+i = n.index(20)
+
+n.count(20)
+
+n.sort()
+n.reverse()
+
+c= n.copy()
+c.pop()
+print(n)
+print(i)
+print("c",c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
