72968c89725662f03f5ceccef20207b7eb3f9383
Shkkhwaja <khwajashaikh703@gmail.com>
2026-07-24 22:57:10 +0530

    tupples

--- part2.py
+++ part2.py
@@ -82,30 +82,105 @@
 
 
 
-n=[10,20,50,40,60]
+# n=[10,20,50,40,60]
 
-n.append(70)
-n.extend([80,90])
-n.insert(0,20)
-n.remove(20)
+# n.append(70)
+# n.extend([80,90])
+# n.insert(0,20)
+# n.remove(20)
 
-n.pop()
-n.pop(2)
-i = n.index(20)
+# n.pop()
+# n.pop(2)
+# i = n.index(20)
 
-n.count(20)
+# n.count(20)
 
-n.sort()
-n.reverse()
+# n.sort()
+# n.reverse()
 
-c= n.copy()
-c.pop()
-print(n)
-print(i)
-print("c",c)
+# c= n.copy()
+# c.pop()
+# print(n)
+# print(i)
+# print("c",c)
+
+
+# // tuples 
 
 
 
+# t= ("Apple","banana","graps","mango","blueberry")
+
+
+# n = (10, 20, 30, 40, 50)
+
+# print(n[0])
+# print(n[-1])
+
+
+# s=("prakash","hussain","faiz","faizan","danish")
+
+# for i in range(len(s)):
+#     print(s[i])
+
+
+# n=(5, 2, 5, 8, 5, 10)
+
+# t=n.count(5)
+
+
+# print(t)
+
+
+# g=("Java", "C", "Python", "JavaScript")
+
+# i = g.index("Python")
+
+# print(i)
+
+
+# f=("Apple", "Banana", "Mango")
+
+# b= False
+
+# for i in range(len(f)):
+#     if f[i] == "Mango":
+#         b= True
+
+# print(b)
+
+
+
+# n=(10, 20, 30, 40, 50)
+
+# i=0
+
+# while i < len(n):
+#     print(n[i])
+#     i +=1
+
+
+# stu = ("khwaja",22,"11-04-2004")
+
+# name,age,dob=stu
+
+# print(name)
+# print(age)
+# print(dob)
+
+
+# f=("Red", "Green", "Blue", "Yellow")
+
+# l = len(f)
+
+# print(l)
+
+
+n=(1,2,3,4,5,6,7,8,9,10)
+
+for i in range(len(n)):
+    if n[i] % 2 ==0:
+        print(n[i])
 
 
 
