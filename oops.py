15832ebed5eb99025a51ada143f8ada419c931a2
shaikh-new <khwajaatzar@gmail.com>
2026-07-30 10:42:06 +0530

    static method

--- oops.py
+++ oops.py
@@ -9,18 +9,62 @@
 # Car.pr()
 
 
-class Stu:
-    fees = 152.22
+# class Stu:
+#     fees = 152.22
 
-    def name(self):
-        print("hussain")
+#     def name(self):
+#         print("hussain")
 
-stu1 = Stu()
-stu2 = Stu()
-print(stu1.fees)
-print(stu2.fees)
+# stu1 = Stu()
+# stu2 = Stu()
+# print(stu1.fees)
+# print(stu2.fees)
 
-stu1.name()
+# stu1.name()
+
+
+
+
+# class Bag:
+#     def __init__(self,material,zip):
+#         self.material = material
+#         self.zip = zip
+        
+
+# bag = Bag("cotton",2)
+
+
+
+class Animal:
+    a=25
+    def __init__(self,name):
+        self.name = name    #instance/object attribute
+    
+    def details(self):      #instance/object method
+        print(f"how are you {self.name}")
+
+    @classmethod   #decorator
+    def det(cls):
+        print(f"class decorator {cls.a}")
+
+    @staticmethod
+    def stamet():
+        print("this is an static method")
+
+obj = Animal("lion")
+
+print(obj.name)
+
+obj.det()        
+obj.stamet()        
+
+
+
+
+
+
+
+
 
 
 
