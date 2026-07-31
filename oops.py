1c72c8540c85c85efe0251e9c2bd086810d61dba
shaikh-new <khwajaatzar@gmail.com>
2026-07-31 11:35:03 +0530

    enheritance

--- oops.py
+++ oops.py
@@ -35,34 +35,84 @@
 
 
 
-class Animal:
-    a=25
-    def __init__(self,name):
-        self.name = name    #instance/object attribute
+# class Animal:
+#     a=25
+#     def __init__(self,name):
+#         self.name = name    #instance/object attribute
     
-    def details(self):      #instance/object method
-        print(f"how are you {self.name}")
+#     def details(self):      #instance/object method
+#         print(f"how are you {self.name}")
 
-    @classmethod   #decorator
-    def det(cls):
-        print(f"class decorator {cls.a}")
+#     @classmethod   #decorator
+#     def det(cls):
+#         print(f"class decorator {cls.a}")
 
-    @staticmethod
-    def stamet():
-        print("this is an static method")
+#     @staticmethod
+#     def stamet():
+#         print("this is an static method")
 
-obj = Animal("lion")
+# obj = Animal("lion")
 
-print(obj.name)
+# print(obj.name)
 
-obj.det()        
-obj.stamet()        
-
-
+# obj.det()        
+# obj.stamet()        
 
 
 
+#inheritance
 
+# class Animal:
+#     a=22
+#     def __init__(self,name):
+#         self.name = name
+
+#     def details(self):
+#         print(f"My name is {self.name}")
+
+# class Human(Animal):
+#     pass
+
+
+# obj1 = Animal("hussain")
+
+# obj2 = Human("shaikh")
+
+# obj1.details()
+# obj2.details()
+# print(obj2.a)
+
+#A inheritance means the child class has all the power of parent class measn it has access for attribute, methods and initiate.
+
+class BagFactory:
+    def __init__(self,material,zip,pockets):
+        self.material = material
+        self.zip = zip
+        self.pockets = pockets
+        
+
+    def details(self):
+        print(f"the bag material is : {self.material}")
+        print(f"the bag ZIP is : {self.zip}")
+        print(f"the bag pockets is : {self.pockets}")
+
+
+
+bag1 = BagFactory("cotton",3,5)
+bag1.details()
+
+
+class Reebok(BagFactory):
+    def __init__(self, material, zip, pockets,colour):
+        super().__init__(material, zip, pockets)
+        self.colour = colour
+        
+    def col(self):
+        print(f"this is the colour {self.colour}")
+
+bag2 = Reebok("Polister",2,4,"gray")
+bag2.details()
+bag2.col()
 
 
 
